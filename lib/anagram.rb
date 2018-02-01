739a2c49034450e5409a1d9d384d55fc1dc9871f
Ron Sala <ron.sala@gmail.com>
2018-02-01 05:37:53 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,11 +8,7 @@ class Anagram
 
   def match(possible_anagrams_ary)
     matches = []
-    possible_anagrams_ary.each do |w|
-      matches << w if w.split("").sort == @word.split("").sort
-
-      # end
-    end
+    possible_anagrams_ary.each {}|w| matches << w if w.split("").sort == @word.split("").sort}
       matches
   end
 
