43198b51358d36b3084deb96bcfd4a646d5043b8
Ron Sala <ron.sala@gmail.com>
2018-02-01 05:33:32 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,9 +2,9 @@ class Anagram
 
   attr_accessor :word
 
-  # def initialize(word)
-  #   @word = word
-  # end
+  def initialize(word)
+    @word = word
+  end
 
   def match(possible_anagrams_ary)
     matches = []
