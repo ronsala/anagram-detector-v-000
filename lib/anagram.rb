1b2d5e6c4a69bc410131ae70bd38b16ab289c2e9
Ron Sala <ron.sala@gmail.com>
2018-02-01 05:19:52 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,11 @@ class Anagram
   end
 
   def match(possible_anagrams_ary)
-
+    possible_anagrams_ary.each do |w|
+      if w.split("").sort == word.split("").sort
+        matches << self
+      end
+      matches
   end
 
 end
